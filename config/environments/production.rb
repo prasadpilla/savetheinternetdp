6e77a97b973c6dee87db136cdd7f83166267629f
Prasad Pilla <prasad.pilla@gmail.com>
2015-12-28 01:05:05 +0530

    Some beautification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  server '52.0.165.53', user: 'deploy', roles: %w{web app db}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+
+  config.assets.precompile += %w( *.css *.js )
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
