a2ceec89eba0e5b5ca24a6aa4d64a9ab89bedf98
Prasad Pilla <prasad.pilla@gmail.com>
2015-12-27 10:19:03 +0530

    Added capistrato for deployment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_savetheinternet_dp_session'
+Rails.application.config.session_store :cookie_store, key: '_savetheinternetdp_session'
