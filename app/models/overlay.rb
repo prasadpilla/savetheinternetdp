252df6615f29f81c4aeba0b4021097b41bb820dd
Prasad Pilla <prasad.pilla@readsup.com>
2015-12-29 23:09:17 +0530

    Index page completly changed
    Added freebasics image
    
    changed opacity to zero (The image is already transperant)
    
    footer edited

--- app/models/overlay.rb
+++ app/models/overlay.rb
@@ -9,7 +9,7 @@ class Overlay
 
     overlay = Magick::Image.read("app/assets/images/overlays/#{overlay_image}").first
     temp = source.composite(overlay, Magick::CenterGravity, Magick::SrcOverCompositeOp)
-    temp.opacity = (Magick::TransparentOpacity-Magick::OpaqueOpacity) * 0.45
+    temp.opacity = (Magick::TransparentOpacity-Magick::OpaqueOpacity) * 0
     source.composite!(temp, Magick::CenterGravity, Magick::SrcOverCompositeOp)
     s3_upload source, file
   end
